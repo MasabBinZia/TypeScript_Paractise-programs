2e4f3d46f2df6c997879b8659352b6bc433977a4
Masab Bin Zia <94193065+MasabBinZia@users.noreply.github.com>
2023-03-31 15:39:17 +0500

    04

---
+++ 14.ts
@@ -0,0 +1,33 @@
+// Define the structure of a guest object
+interface Guest {
+    name: string;
+    email: string;
+  }
+  
+  // Create an array of guests
+  const guestList: Guest[] = [
+    {
+      name: "Albert Einstein",
+      email: "einstein@example.com"
+    },
+    {
+      name: "Ada Lovelace",
+      email: "lovelace@example.com"
+    },
+    {
+      name: "Nelson Mandela",
+      email: "mandela@example.com"
+    }
+  ];
+  
+  // Define a function to send an invitation to a guest
+  function sendInvitation(guest: Guest): void {
+    console.log(`Dear ${guest.name},\n\nYou are cordially invited to dinner at my place on Friday evening. Please let me know if you can make it by replying to this email.\n\nBest regards,\nJohn Doe`);
+  }
+  
+  // Iterate over the guest list and send an invitation to each guest
+  for (let i = 0; i < guestList.length; i++) {
+    const guest = guestList[i];
+    sendInvitation(guest);
+  }
+  
---
+++ JS/15.js
@@ -0,0 +1,30 @@
+// Create an array of guests
+let guestList = [
+    {
+        name: "Albert Einstein",
+        email: "einstein@example.com"
+    },
+    {
+        name: "Ada Lovelace",
+        email: "lovelace@example.com"
+    },
+    {
+        name: "Nelson Mandela",
+        email: "mandela@example.com"
+    }
+];
+// Print the name of the guest who can't make it
+const guestWhoCantMakeIt = guestList[1].name;
+console.log(`${guestWhoCantMakeIt} can't make it to dinner.`);
+// Modify the guest list
+guestList[1].name = "Marie Curie";
+guestList[1].email = "curie@example.com";
+// Define a function to send an invitation to a guest
+function sendInvitation(guest) {
+    console.log(`Dear ${guest.name},\n\nYou are cordially invited to dinner at my place on Friday evening. Please let me know if you can make it by replying to this email.\n\nBest regards,\nJohn Doe`);
+}
+// Iterate over the guest list and send an invitation to each guest
+for (let i = 0; i < guestList.length; i++) {
+    const guest = guestList[i];
+    sendInvitation(guest);
+}
